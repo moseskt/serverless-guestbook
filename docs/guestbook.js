ebdbad9e9e5f9adf25ec6fec5f25b9dddc6ab15f
Moses Tapfuma <mktapfuma@gmail.com>
2020-07-04 12:44:28 +0200

    Updated guestbook.js
    Updated guestbook.js with the correct apiUrl

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://CHANGEME.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://9b33f2e6.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
